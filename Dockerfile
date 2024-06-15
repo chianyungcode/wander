f7b289192e296dd75da0630ea7dfe42bf9ed1e36
chianyungcode <cnytechcode@gmail.com>
2024-06-16 03:33:38 +0700

    feat: setup docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM oven/bun:latest
+
+WORKDIR /usr/app
+
+ENV DATABASE_URL=URL
+ENV DIRECT_URL=URL
+ENV APP_PORT=3000
+
+COPY . .
+
+RUN bun install
+
+EXPOSE ${APP_PORT}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+
 import destinations from "./routes/destinations";
 import locations from "./routes/location";
 import owners from "./routes/owner";
-import { logger } from "hono/logger";
 
 const app = new Hono().basePath("/api");
 app.use(logger());
@@ -11,4 +12,7 @@ app.route("/destinations", destinations);
 app.route("/locations", locations);
 app.route("/owners", owners);
 
-export default app;
+export default {
+  port: process.env.APP_PORT,
+  fetch: app.fetch,
+};
