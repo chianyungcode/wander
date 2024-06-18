4e8b76b34008b979782a3ec047fb2fa29af245b0
chianyungcode <cnytechcode@gmail.com>
2024-06-18 19:00:26 +0700

    chore(docker): update setup

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,12 @@ WORKDIR /usr/src/app
 
 ENV DATABASE_URL=URL
 ENV DIRECT_URL=URL
-ENV PORT=3000
+ENV APP_PORT=3000
 
 COPY . .
 
 RUN bun install
 
-EXPOSE ${PORT}
+EXPOSE ${APP_PORT}
 
 CMD ["bun", "run", "start"]
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,17 @@ import destinations from "./routes/destinations";
 import locations from "./routes/location";
 import owners from "./routes/owner";
 
-const app = new Hono().basePath("/api");
+const app = new Hono();
 app.use(logger());
 
-app.route("/destinations", destinations);
-app.route("/locations", locations);
-app.route("/owners", owners);
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello from root",
+  });
+});
+app.route("api/destinations", destinations);
+app.route("api/locations", locations);
+app.route("api/owners", owners);
 
 export default {
   port: process.env.APP_PORT,
